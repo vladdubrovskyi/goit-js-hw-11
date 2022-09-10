eefe1b2a53bd77efb6cb31ecb4d44f4477c0ba63
vladdubrovskyi <vladdubrovskyi1993@gmail.com>
2022-09-10 19:16:11 +0300

    adds basic logic
    1. Added request-maker
    2. Added notifications about succsess and failed requests.
    3. Added basic markup

---
+++ src/imgapi.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+export default class ImgApiSrv {
+  constructor() {
+    this.SearchQuery = '';
+    this.page = 1;
+  }
+
+  getImages() {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const searchParams = new URLSearchParams({
+      key: '29821254-cc8d55b85aa42c363f8211fb8',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      per_page: 40,
+    });
+    return axios(
+      `${BASE_URL}?${searchParams}&q=${this.SearchQuery}&page=${this.page}`
+    )
+      .then(res => {
+        if (res.data.hits.length > 0) {
+          this.page += 1;
+          const totalHits = res.data.totalHits;
+          this.totalHits = totalHits;
+          Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+          return res.data.hits;
+        }
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return [];
+      })
+      .catch(err => console.log(err));
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.SearchQuery;
+  }
+  set query(newSearchQueary) {
+    return (this.SearchQuery = newSearchQueary);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,98 @@
-import Axios from 'axios';
+import axios from 'axios';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import ImgApiSrv from './imgapi';
+
+const ApiSrv = new ImgApiSrv();
 
 const formEl = document.querySelector('#search-form');
-console.log(formEl);
+const inputEl = document.querySelector('[type="text"]');
+const galleryContainer = document.querySelector('.gallery');
+const loadBtnEl = document.querySelector('.load-btn');
+// const BASE_URL = 'https://pixabay.com/api/';
+// const searchParams = new URLSearchParams({
+//   key: '29821254-cc8d55b85aa42c363f8211fb8',
+//   image_type: 'photo',
+//   orientation: 'horizontal',
+//   safesearch: 'true',
+//   per_page: 4,
+//   page: 1,
+// });
+// let searchContext = '';
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  ApiSrv.query = e.currentTarget.elements.searchQuery.value;
+  if (ApiSrv.query === '') {
+    return;
+  }
+  ApiSrv.resetPage();
+  ApiSrv.getImages().then(renderPage);
+  clearContainer();
+
+  //   searchContext = e.currentTarget.elements.searchQuery.value;
+  //     .axios(`${BASE_URL}?${searchParams}&q=${searchContext}`)
+  //     .then(res => {
+  //       return res.data.hits;
+  //     })
+  //     .then(renderPage)
+  //     .catch(err => console.log(err));
+  loadBtnEl.classList.remove('is-hidden');
+}
+
+loadBtnEl.addEventListener('click', onLoadBtn);
+
+function onLoadBtn(e) {
+  ApiSrv.getImages().then(renderPage);
+
+  //   console.log(searchParams);
+  //   axios(`${BASE_URL}?${searchParams}&q=${searchContext}`)
+  //     .then(res => {
+  //       return res.data.hits;
+  //     })
+  //     .then(renderPage)
+  //     .catch(err => console.log(err));
+}
+
+function renderPage(data) {
+  const markup = data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>:${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>:${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>:${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>:${downloads}
+    </p>
+  </div>
+</div>`;
+      }
+    )
+    .join('');
+  return galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearContainer() {
+  galleryContainer.innerHTML = '';
+}
