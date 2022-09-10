922cf370b74ed5befbd1cfe23066456a6f0ac044
vladdubrovskyi <vladdubrovskyi1993@gmail.com>
2022-09-10 10:01:33 +0300

    adds additional starting settings

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Axios from 'axios';
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const formEl = document.querySelector('#search-form');
+console.log(formEl);
